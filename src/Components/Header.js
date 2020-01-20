baadcb098ad35d68a517a4b7b5e436b79e321263
Kori Gandolph <korigandolph@Koris-MacBook-Air.local>
2020-01-20 14:03:20 -0700

    more styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,17 +3,29 @@ import React from 'react';
 const Header = ()=> {
     return(
         <header id = "header">
-            <img id='nba-logo' src='https://cdn.freebiesupply.com/images/large/2x/nba-logo-transparent.png' alt ='nba logo'/>
             <nav id='header-words'>
+            <img id='nba-logo' src='https://cdn.freebiesupply.com/images/large/2x/nba-logo-transparent.png' alt ='nba logo'/>
                 <a>NBA</a>
             </nav>
-            <nav id="header-header">
+            <nav id="menu">
                 <a>Scores </a>
                 <a>Schedule </a>
                 <a>News </a>
                 <a>Video </a>
                 <a>NBA TV </a>
+                <a>Stats </a>
+                <a>Standings </a>
+                <a>Players </a>
+                <a>Teams </a>
+                <a>All-Star </a>
             </nav>
+            <nav>
+                <a>Pick 'Em </a>
+                <a>Shop </a>
+                <a>Tickets </a>
+                <a>NBA League Pass </a>
+            </nav>
+                <img id="login" src ="https://www.materialui.co/materialIcons/action/account_circle_white_192x192.png" alt="login"/>
         </header>
     )
 }
